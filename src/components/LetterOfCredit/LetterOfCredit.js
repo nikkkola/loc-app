9a7eb61826a7117f48f1c14ab4289ba135a02909
Nikola Ignatov <nik.ignatovv@gmail.com>
2018-03-08 11:35:30 +0000

    Added components folder and LoC component

---
+++ src/components/LetterOfCredit/LetterOfCredit.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './letterofcredit.css';
+
+class LetterOfCredit extends Component {
+  render() {
+    return (<div>LoC</div>);
+  }
+}
+
+LetterOfCredit.propTypes = {
+  name: PropTypes.string
+};
+
+export default LetterOfCredit;
