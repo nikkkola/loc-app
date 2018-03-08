7b8267757be41fca6bbb5a76dc1364ffecfed9c3
Erin Hughes <erinhughes@erins-mbp.hursley.uk.ibm.com>
2018-03-08 13:36:50 +0000

    Added a basic page component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page from './components/Page/Page.js';
 import LetterOfCredit from './components/LetterOfCredit/LetterOfCredit.js';
 
 class App extends Component {
   render() {
+
+    let alice = {
+      "$class": "org.acme.loc.Customer",
+      "companyName": "QuickFix IT",
+      "personId": "alice",
+      "name": "Alice",
+      "bankName": "Bank of Argentina"
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <LetterOfCredit/>
-        </p>
-      </div>
+      <Page currentUser={alice.name} userBank={alice.bankName} contents={<LetterOfCredit />}/>
     );
   }
 }
